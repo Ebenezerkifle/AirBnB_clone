27d5dadaea03bd1c9c1185a525db31c11c4c5038
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-08-10 16:30:19 +0300

    init method is added to the basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -2,14 +2,14 @@
 # base_model.py
 """This is a base model class from which all other classes inherits
 """
-import datetime
+from datetime import datetime
 import uuid
 
 
 class BaseModel:
     """Represent the base model."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Represents the "base" for all other classes in this project
     
          Attributes:
@@ -21,14 +21,27 @@ class BaseModel:
             - *args: list of arguments
             - **kwargs: dict of key-alues arguments
     """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        if kwargs:
+            dtime_format = '%Y-%m-%dT%H:%M:%S.%f'
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(kwargs['updated_at'], dtime_format)
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(kwargs['created_at'], dtime_format)
+                else:
+                    setattr(self, key, value)
+        else:
+            """creating a new instance of a class"""
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """Updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """Returns a dictionary that contains all
@@ -40,4 +53,8 @@ class BaseModel:
         return my_dict
 
     def __str__(self):
-        return "[BaseModel] ({}) {}".format(self.id, self.__dict__)
+        """Returns a readable string representation
+        of BaseModel instances"""
+
+        clsName = self.__class__.__name__
+        return "[{}] ({}) {}".format(clsName, self.id, self.__dict__)
