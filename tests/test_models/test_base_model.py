376d0fb142ab3ae69f993567086edeb80b09d3b6
Abeniezer Kifle <49494895+Ebenezerkifle@users.noreply.github.com>
2023-08-12 17:41:01 +0300

    unit test file is added for base model

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines unittests for models/base_model.py.
+Unittest classes:
+    TestBaseModel_instantiation
+    TestBase_Instance_Print
+    TestBaseModel_save
+    TestBase_from_json_string
+    TestBaseModel_to_dict
+"""
+import unittest
+
+
+class TestBaseModel_Instantiation(unittest.TestCase):
+    pass
